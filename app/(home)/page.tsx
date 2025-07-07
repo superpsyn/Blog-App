02705e96452b773236ff6e19e7d456de6e1cce2a
superpsyn <nithinbharadwaj8@gmail.com>
2025-07-07 13:28:14 -0600

    Create hero section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "@/components/home/header/navbar"
+import HeroSection from '@/components/home/hero-section';
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
