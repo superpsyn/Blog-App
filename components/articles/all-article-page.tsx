b3973c40394c013309d4e92219410a34e3b638e5
superpsyn <nithinbharadwaj8@gmail.com>
2025-07-10 10:44:53 -0600

    add minor fix to all-article-page

--- components/articles/all-article-page.tsx
+++ components/articles/all-article-page.tsx
@@ -55,7 +55,7 @@ const AllArticlePage: React.FC<AllArticlePageProps> = async ({
                 <h3 className="mt-4 text-xl font-semibold text-gray-900 dark:text-white">
                   {article.title}
                 </h3>
-                <p className="mt-2 text-gray-600 dark:text-gray-300">
+                <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
                   {article.category.toUpperCase()}
                 </p>
   
